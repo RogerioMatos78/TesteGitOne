9bd61dafe6c925efdc64e4d6b7dfece4ded2bb7a
RogerioMatos78 <rogerio.matos78@gmail.com>
2023-02-22 10:25:22 -0300

    segundo teste git

--- TesteGit/src/testeGit/TesteGit.java
+++ TesteGit/src/testeGit/TesteGit.java
@@ -4,6 +4,14 @@ public class TesteGit {
 
 	public static void main(String[] args) {
 		System.out.println("Teste");
+		
+		AlunoClass aluno = new AlunoClass();
+		
+		aluno.setNome("Teste Git");
+		aluno.setIdade(45);
+		
+		System.out.println("Nome " + aluno.getNome() );
+		System.out.println("Idade " + aluno.getIdade() );		
 	}
 
 }
